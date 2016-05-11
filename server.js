62a6784010bbefcdaec9d77775de5fa144dbc322
Aditya Wate <aditya.wate@hotmail.com>
2016-05-11 07:43:17 -0400

    publishing to OpenShift and Git

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
-/**
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-**/
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port,ipaddress);
+require ("./test/app.js")(app);
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+
+app.listen(port, ipaddress);
