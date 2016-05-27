588baa3dc76ee5df3754875a8b012eb120388091
Aditya Wate <aditya.wate@hotmail.com>
2016-05-27 19:49:48 -0400

    Added dynamic alerts to profile

--- public/assignment/views/user/profile.controller.client.js
+++ public/assignment/views/user/profile.controller.client.js
@@ -6,6 +6,7 @@
     function ProfileController($routeParams, UserService) {
         var vm = this;
         vm.updateUser = updateUser;
+        vm.closeAlert = closeAlert;
 
         var id = $routeParams.id;
 
@@ -15,7 +16,20 @@
         init();
 
         function updateUser(newUser) {
-            UserService.updateUser(id, newUser);
+            userUpdated = UserService.updateUser(id, newUser);
+
+            if(userUpdated){
+                vm.alertType = "Success";
+                vm.alert = "Your profile was saved.";
+            }
+            else{
+                vm.alertType = "Failure";
+                vm.alert = "Profile was not updated.";
+            }
+        }
+
+        function closeAlert() {
+            vm.alertType = null;
         }
     }
 
