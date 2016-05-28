879345027fa941624c198404dc673935ea5cba20
Aditya Wate <aditya.wate@hotmail.com>
2016-05-28 00:13:15 -0400

    Created remaining api functions

--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -15,21 +15,14 @@
     function WebsiteService() {
         var api = {
             createWebsite: createWebsite,
-            findWebsitesForUserId: findWebsitesForUserId,
+            findWebsitesByUser: findWebsitesByUser,
+            findWebsiteById: findWebsiteById,
+            updateWebsite: updateWebsite,
             deleteWebsite: deleteWebsite
         };
         return api;
 
-        function deleteWebsite(websiteId) {
-            for(var i in websites) {
-                if(websites[i]._id === websiteId) {
-                    websites.splice(i, 1);
-                    return true;
-                }
-            }
-            return false;
-        }
-        
+
         function createWebsite(developerId, name, desc) {
             var newWebsite = {
                 _id: (new Date()).getTime()+"",
@@ -41,7 +34,7 @@
             return newWebsite;
         }
 
-        function findWebsitesForUserId(userId) {
+        function findWebsitesByUser(userId) {
             var resultSet = [];
             for(var i in websites) {
                 if(websites[i].developerId === userId) {
@@ -49,6 +42,37 @@
                 }
             }
             return resultSet;
+        }
+
+        function findWebsiteById(websiteId){
+            var website;
+            for(var i in websites) {
+                if(websites[i]._id === websiteId) {
+                    website = websites[i];
+                }
+            }
+            return website;
+        }
+
+        function updateWebsite(websiteId, website){
+            for(var i in websites) {
+                if(websites[i]._id === websiteId) {
+                    websites[i].name = website.name;
+                    websites[i].description = website.description;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        function deleteWebsite(websiteId) {
+            for(var i in websites) {
+                if(websites[i]._id === websiteId) {
+                    websites.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
         }
     }
 })();
--- public/assignment/views/website/website-edit.controller.client.js
+++ public/assignment/views/website/website-edit.controller.client.js
@@ -8,7 +8,20 @@
         vm.userId = $routeParams.userId;
         vm.websiteId = $routeParams.websiteId;
         vm.deleteWebsite = deleteWebsite;
+        vm.updateWebsite = updateWebsite;
 
+        function init() {
+            vm.website = WebsiteService.findWebsiteById(vm.websiteId);
+        }
+        init();
+        function updateWebsite(websiteId, website) {
+            var result = WebsiteService.updateWebsite(websiteId, website);
+            if(result) {
+                $location.url("/user/"+vm.userId+"/website");
+            } else {
+                vm.error = "Unable to update website";
+            }
+        }
         function deleteWebsite(websiteId) {
             var result = WebsiteService.deleteWebsite(websiteId);
             if(result) {
