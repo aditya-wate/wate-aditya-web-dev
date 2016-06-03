8f90c997f3c0d4879d72fc29c92b3c10e12d29b8
Aditya Wate <aditya.wate@hotmail.com>
2016-06-03 02:25:00 -0400

    website service for server created

--- assignment/services/website.service.server.js
+++ assignment/services/website.service.server.js
@@ -16,8 +16,18 @@ module.exports = function(app) {
     app.delete("/api/website/:websiteId", deleteWebsite);
 
     function createWebsite(req, res){
-
+        var userId = req.params.userId;
+        var website = req.body;
+        var newWebsite = {
+            _id: (new Date()).getTime()+"",
+            name: website.name,
+            description: website.desc,
+            developerId: userId
+        };
+        websites.push(newWebsite);
+        res.json(newWebsite);
     }
+
     function findAllWebsitesForUser(req, res){
         var userId = req.params.userId;
         var result = [];
@@ -28,13 +38,41 @@ module.exports = function(app) {
         }
         res.json(result);
     }
+
     function findWebsiteById(req, res){
-
+        var websiteId = req.params.websiteId;
+        for(var i in websites) {
+            if(websites[i]._id === websiteId) {
+                res.json(websites[i]);
+                return;
+            }
+        }
+        res.json({});
     }
+
     function updateWebsite(req, res){
+        var id = req.params.websiteId;
+        var website = req.body;
 
+        for(var i in websites) {
+            if(websites[i]._id === id) {
+                websites[i].name = website.name;
+                websites[i].description = website.description;
+                res.send(200);
+            }
+        }
+        res.send(400);
     }
+    
     function deleteWebsite(req, res){
-
+        var id = req.params.websiteId;
+        for(var i in websites) {
+            if (websites[i]._id === id) {
+                websites.splice(i, 1);
+                res.send(200);
+                return;
+            }
+        }
+        res.send(400);
     }
 };
