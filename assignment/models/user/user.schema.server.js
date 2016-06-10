db35263cb1b289081767c1da308a716b9949215b
Aditya Wate <aditya.wate@hotmail.com>
2016-06-10 15:49:23 -0400

    user schema formatting

--- assignment/models/user/user.schema.server.js
+++ assignment/models/user/user.schema.server.js
@@ -2,17 +2,34 @@
 module.exports = function() {
     var mongoose = require("mongoose");
 
-    var UserSchema = mongoose.Schema({
-        username: {type: String, required: true},
-        password: String,
-        firstName: String,
-        lastName: String,
-        email: String,
-        phone: String,
-        // dob: Date,
-        websites: {type: Array, "default": []},
-        dateCreated: {type: Date, default: Date.now}
-    }, {collection: "assignment.user"});
+    var UserSchema = mongoose.Schema(
+        {
+            username:
+            {
+                type: String,
+                required: true
+            },
+            password: String,
+            firstName: String,
+            lastName: String,
+            email: String,
+            phone: String,
+            // dob: Date,
+            websites:
+            {
+                type: Array,
+                "default": []
+            },
+            dateCreated:
+            {
+                type: Date,
+                default: Date.now
+            }
+        },
+        {
+            collection: "assignment.user"
+        }
+    );
 
     return UserSchema;
 };
