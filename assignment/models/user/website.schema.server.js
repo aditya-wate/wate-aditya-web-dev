1039d040b4e7064affdb965fa1d6c8af16c64951
Aditya Wate <aditya.wate@hotmail.com>
2016-06-10 15:50:30 -0400

    website schema formatting

--- assignment/models/user/website.schema.server.js
+++ assignment/models/user/website.schema.server.js
@@ -1,13 +1,29 @@
 module.exports = function() {
     var mongoose = require("mongoose");
 
-    var Website = mongoose.Schema({
-        _user: {type: mongoose.Schema.ObjectId, ref: "User"},
-        name: String,
-        description: String,
-        pages: {type: Array, "default": []},
-        dateCreated: {type: Date, default: Date.now}
-    }, {collection: "assignment.website"});
+    var Website = mongoose.Schema(
+        {
+            _user:
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: "User"
+            },
+            name: String,
+            description: String,
+            pages:
+            {
+                type: Array,
+                "default": []
+            },
+            dateCreated:
+            {type: Date,
+                default: Date.now
+            }
+        },
+        {
+            collection: "assignment.website"
+        }
+    );
 
     return Website;
 };
