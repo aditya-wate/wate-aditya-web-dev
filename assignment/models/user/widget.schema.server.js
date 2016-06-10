7c986174601b0e8d0a4e8efc093967e7d00b3f1c
Aditya Wate <aditya.wate@hotmail.com>
2016-06-10 15:47:04 -0400

    Widget Schema: first draft

---
+++ assignment/models/user/widget.schema.server.js
@@ -0,0 +1,39 @@
+module.exports = function() {
+    var mongoose = require("mongoose");
+
+    var Widget = mongoose.Schema(
+        {
+            _page:
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: "Page"
+            },
+            type:
+            {
+                type: String,
+                enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT']
+            },
+            name: String,
+            text: String,
+            placeholder: String,
+            description: String,
+            url: String,
+            width: String,
+            height: String,
+            rows: Number,
+            size: Number,
+            class: String,
+            icon: String,
+            deletable: Boolean,
+            formatted: Boolean,
+            dateCreated:
+            {
+                type: Date,
+                default: Date.now
+            }
+        },
+        {collection: "assignment.page"}
+    );
+
+    return Widget;
+};
