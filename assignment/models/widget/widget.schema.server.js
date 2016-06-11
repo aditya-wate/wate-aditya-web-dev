59dd59efe7496dee5c1c8a2288eb42226009f12f
Aditya Wate <aditya.wate@hotmail.com>
2016-06-11 15:59:01 -0400

    text widget: first draft

--- assignment/models/widget/widget.schema.server.js
+++ assignment/models/widget/widget.schema.server.js
@@ -11,7 +11,7 @@ module.exports = function() {
             type:
             {
                 type: String,
-                enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT']
+                enum: ['HEADING', 'IMAGE', 'YOUTUBE', 'HTML', 'INPUT','TEXT']
             },
             name: String,
             text: String,
