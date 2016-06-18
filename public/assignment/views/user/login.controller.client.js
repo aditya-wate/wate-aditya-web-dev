8b2a46b3a9ac2b886c2de4d41e726635fc8fb3b7
Aditya Wate <aditya.wate@hotmail.com>
2016-06-18 14:00:52 -0400

    submit not allowed of not valid

--- public/assignment/views/user/login.controller.client.js
+++ public/assignment/views/user/login.controller.client.js
@@ -8,56 +8,58 @@
         vm.login = login;
 
         function login(user) {
-            UserService
-                .login(user)
-                .then(
-                    function(response) {
-                        var user = response.data;
-                        $rootScope.currentUser = user;
-                        $location.url("/user/"+user._id);
-                    },
-                    function(error){
-                        vm.error = "User not found";
-                    })
+            if(user)
+                if(user.username && user.password)
+                    UserService
+                        .login(user)
+                        .then(
+                            function(response) {
+                                var user = response.data;
+                                $rootScope.currentUser = user;
+                                $location.url("/user/"+user._id);
+                            },
+                            function(error){
+                                vm.error = "User not found";
+                            })
         }
 
-            /*
-                    function login(username, password) {
-                        // if(username && password)
-                        if (username && password) {
-                            UserService
-                                .findUserByCredentials(username, password)
-                                .then(function(response){
-                                    var user = response.data;
-                                    if(user) {
-                                        $location.url("/user/" + user._id);
-                                    } else {
-                                        vm.error = "User not found";
-                                    }
-                                });
-                            vm.invalidUsername = false;
-                            vm.invalidPassword = false;
-                        }
+        /*
+         function login(username, password) {
+         // if(username && password)
+         if (username && password) {
+         UserService
+         .findUserByCredentials(username, password)
+         .then(function(response){
+         var user = response.data;
+         if(user) {
+         $location.url("/user/" + user._id);
+         } else {
+         vm.error = "User not found";
+         }
+         });
+         vm.invalidUsername = false;
+         vm.invalidPassword = false;
+         }
 
-                        else{
-                            if(!username && password){
-                                vm.error = "Please enter username";
-                                vm.invalidUsername = true;
-                                vm.invalidPassword = false;
-                            }
-                            else if(!password && username) {
-                                vm.error = "Please enter password";
-                                vm.invalidPassword = true;
-                                vm.invalidUsername = false;
-                            }
-                            else {
-                                vm.error = "Please enter username and password";
-                                vm.invalidUsername = true;
-                                vm.invalidPassword = true;
-                            }
-                        }
+         else{
+         if(!username && password){
+         vm.error = "Please enter username";
+         vm.invalidUsername = true;
+         vm.invalidPassword = false;
+         }
+         else if(!password && username) {
+         vm.error = "Please enter password";
+         vm.invalidPassword = true;
+         vm.invalidUsername = false;
+         }
+         else {
+         vm.error = "Please enter username and password";
+         vm.invalidUsername = true;
+         vm.invalidPassword = true;
+         }
+         }
 
-                    }
-                    */
+         }
+         */
     }
 })();
